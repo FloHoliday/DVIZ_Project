440eba346c2cec265fd20a653e20d3b77cfbbc33
Finn Eyer <finn.eyer24@gmail.com>
2025-01-02 17:41:22 +0100

    [TASK] Cleaned callback functions and comments

--- Dashbord/corr_graph_functions.py
+++ Dashbord/corr_graph_functions.py
@@ -1,12 +1,14 @@
 import plotly.graph_objects as go
 
-def get_corr_graph(mh_data, disorder, indicator, country_code, country_name ,colors):
+def get_corr_graph(mh_data, disorder, indicator, country_code ,colors):
+    # Extract data for the selected country and disorder
     mh_data_country = mh_data[mh_data['Code'] == country_code]
     mh_data_disorder = mh_data_country[['Year', disorder]]
     mh_data_indicator = mh_data_country[['Year', indicator]]
     mh_display_name = disorder.replace('_', ' ').capitalize()
     indicator_title = ''
 
+    # Set the indicator title with its unit
     match indicator:
         case 'gdp':
             indicator_title = 'GDP (in billion USD)'
@@ -53,10 +55,10 @@ def get_corr_graph(mh_data, disorder, indicator, country_code, country_name ,col
         # Axes
         xaxis=dict(
             title='Year',
-            showgrid=False,  # No grid lines for a clean look
-            showline=True,  # Show axis lines
-            linecolor='black',  # Axis line color
-            ticks='outside',  # Ticks pointing outward
+            showgrid=False,  
+            showline=True,  
+            linecolor='black',
+            ticks='outside',
             tickcolor='black',
             tickfont=dict(
                 size=12,
@@ -102,10 +104,9 @@ def get_corr_graph(mh_data, disorder, indicator, country_code, country_name ,col
                 color=colors['blush-pink']
             )
         ),
-        
         # Legend
         legend=dict(
-            orientation='h',  # Horizontal layout for the legend
+            orientation='h',
             x=0.5,
             xanchor='center',
             y=-0.2,
@@ -117,14 +118,14 @@ def get_corr_graph(mh_data, disorder, indicator, country_code, country_name ,col
         
         # Margins
         margin=dict(
-            l=50,  # Left margin
-            r=50,  # Right margin
-            t=50,  # Top margin
-            b=40   # Bottom margin
+            l=50,
+            r=50,
+            t=50,
+            b=40 
         ),
         
         # Template
-        template='simple_white',  # Clean white template
+        template='simple_white',
     )
     return fig
     
